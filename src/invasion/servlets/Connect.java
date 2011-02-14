8e7011c3bfeea09f3db8ff7b3596228c3ee02da0
Jeffrey Hoyt <jchoyt@gmail.com>
2011-02-14 18:20:25 -0500

    removed a couple more java code-only jsps

---
+++ src/invasion/servlets/Connect.java
@@ -0,0 +1,107 @@
+/*
+ *  Copyright 2010 Jeffrey Hoyt.  All rights reserved.
+ */
+
+package invasion.servlets;
+
+import invasion.util.*;
+import invasion.dataobjects.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.sql.*;
+import org.json.*;
+
+/**
+ *
+ *
+ * @author     jchoyt
+ * @created
+ */
+@WebServlet(urlPatterns = { "/connect" } )
+public class Connect extends HttpServlet
+{
+
+    public final static String KEY = Connect.class.getName();
+    public final static Logger log = Logger.getLogger( KEY );
+    // static{log.setLevel(Level.FINER);}
+
+    /**
+     *  Constructor for the PqmServlet object
+     *
+     * @since
+     */
+    public Connect()
+    {
+        super();
+    }
+
+
+
+    /**
+     *  Description of the Method
+     *
+     * @param  config                Description of the Parameter
+     * @exception  ServletException  Description of the Exception
+     */
+    public void init( ServletConfig config )
+        throws ServletException
+    {
+        log.entering( KEY, "init" );
+        /*
+         *  required for all Servlets
+         */
+        super.init( config );
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+        PrintWriter out = response.getWriter();
+        String charId = WebUtils.getRequiredParameter(request, "id");
+        Alt alt = null;
+        int altid = Integer.parseInt(charId);
+        try
+        {
+            alt = Alt.load( altid );
+            if( !alt.getUsername().equals(request.getRemoteUser()) )
+            {
+                response.sendRedirect("/naughty.jsp");
+                return;
+            }
+            Whatzit wazzit = new Whatzit(altid);
+            wazzit.reload();
+            request.getSession().setAttribute( Whatzit.KEY,  wazzit );
+            response.sendRedirect( "/map/index.jsp" );
+        }
+        catch(Exception e)
+        {
+            log.throwing(KEY, "Body", e);
+            response.sendRedirect( "/index.jsp" );
+        }
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+            doGet( request, response );
+    }
+
+}
+
+
---
+++ src/invasion/servlets/Logout.java
@@ -0,0 +1,87 @@
+/*
+ *  Copyright 2010 Jeffrey Hoyt.  All rights reserved.
+ */
+
+package invasion.servlets;
+
+import invasion.util.*;
+import invasion.dataobjects.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.sql.*;
+import org.json.*;
+
+/**
+ *
+ *
+ * @author     jchoyt
+ * @created
+ */
+@WebServlet(urlPatterns = { "/logout" } )
+public class Logout extends HttpServlet
+{
+
+    public final static String KEY = Logout.class.getName();
+    public final static Logger log = Logger.getLogger( KEY );
+    // static{log.setLevel(Level.FINER);}
+
+    /**
+     *  Constructor for the PqmServlet object
+     *
+     * @since
+     */
+    public Logout()
+    {
+        super();
+    }
+
+
+
+    /**
+     *  Description of the Method
+     *
+     * @param  config                Description of the Parameter
+     * @exception  ServletException  Description of the Exception
+     */
+    public void init( ServletConfig config )
+        throws ServletException
+    {
+        log.entering( KEY, "init" );
+        /*
+         *  required for all Servlets
+         */
+        super.init( config );
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+        request.getSession().invalidate();
+        response.sendRedirect( "/index.jsp" );
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+            doGet( request, response );
+    }
+
+}
+
+
