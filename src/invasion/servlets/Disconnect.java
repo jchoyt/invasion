2236cbb8f1e7454218e3f33ddc5ab6624dd15c10
Jeffrey Hoyt <jchoyt@rusalka.(none)>
2011-03-01 23:12:20 -0500

    removed relative paths, fixed bug where having autocommit off was causing db to lock up

---
+++ src/invasion/servlets/Disconnect.java
@@ -0,0 +1,68 @@
+/*
+ *  Copyright 2010 Jeffrey Hoyt.  All rights reserved.
+ */
+
+package invasion.servlets;
+
+import invasion.util.*;
+import invasion.dataobjects.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.sql.*;
+import org.json.*;
+
+/**
+ *
+ *
+ * @author     jchoyt
+ * @created
+ */
+@WebServlet(urlPatterns = { "/disconnect" } )
+public class Disconnect extends HttpServlet
+{
+
+    public final static String KEY = Disconnect.class.getName();
+    public final static Logger log = Logger.getLogger( KEY );
+    // static{log.setLevel(Level.FINER);}
+
+    /**
+     *  Constructor for the PqmServlet object
+     *
+     * @since
+     */
+    public Disconnect()
+    {
+        super();
+    }
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+        request.getSession().removeAttribute(Whatzit.KEY);
+        response.sendRedirect( "/index.jsp" );
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+            doGet( request, response );
+    }
+
+}
+
+
---
+++ src/invasion/servlets/Smite.java
@@ -0,0 +1,94 @@
+/*
+ *  Copyright 2010 Jeffrey Hoyt.  All rights reserved.
+ */
+
+package invasion.servlets;
+
+import invasion.util.*;
+import invasion.dataobjects.*;
+import java.io.*;
+import java.util.*;
+import java.util.logging.*;
+import javax.servlet.*;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.*;
+import java.sql.*;
+import org.json.*;
+
+/**
+ *
+ *
+ * @author     jchoyt
+ * @created
+ */
+@WebServlet(urlPatterns = { "/admin/smite" } )
+public class Smite extends HttpServlet
+{
+
+    public final static String KEY = Smite.class.getName();
+    public final static Logger log = Logger.getLogger( KEY );
+    // static{log.setLevel(Level.FINER);}
+
+    /**
+     *  Constructor for the PqmServlet object
+     *
+     * @since
+     */
+    public Smite()
+    {
+        super();
+    }
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doGet( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+        PrintWriter out = response.getWriter();
+        String altidString = WebUtils.getRequiredParameter(request, "id");
+        String message = WebUtils.getRequiredParameter(request, "message");
+        String query = "update alt set hp=0, lasthurtby=null where id=?";
+        InvasionConnection conn = null;
+        try{
+            int altid = Integer.parseInt(altidString);
+            conn = new InvasionConnection();
+            Alt alt = Alt.load( conn, altid );
+            PreparedStatement ps = conn.prepareStatement(query);
+            ps.setInt(1, altid);
+            int count = ps.executeUpdate();
+            new Message( conn, altid, Message.EFFECT, message );
+            alt.kill( conn);
+            //LATER - lock account
+        }
+        catch(SQLException e)
+        {
+            log.throwing(KEY, "Error smiting someone", e);
+            throw new ServletException(e);
+        }
+        finally
+        {
+            DatabaseUtility.close(conn);
+        }
+        response.setHeader("HTTP-EQUIV","Refresh");
+        response.setHeader("CONTENT", "1;URL=charList.jsp");
+        out.write("<html><body>He/she/it's been smote <br/><a href=\"/admin/charList.jsp\">Back to the character list</a></body></html>");
+    }
+
+
+    /**
+     *  Description of the Method
+     *
+     */
+    @Override
+    public void doPost( HttpServletRequest request, HttpServletResponse response )
+        throws IOException, ServletException
+    {
+            doGet( request, response );
+    }
+
+}
+
+
