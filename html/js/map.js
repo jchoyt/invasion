ebf514132e03f84759c81fde79b8d4ae4adeebb1
Kyte <mauro.salinasa@gmail.com>
2011-03-16 00:31:44 -0400

    jQuery CAN pull multiple tags on the same selector, just fyi

--- html/js/map.js
+++ html/js/map.js
@@ -19,7 +19,7 @@ $(document).ready( function() {
         autoHeight: false
     });
 
-    $("#west-sections").addClass("ui-accordion ui-widget ui-helper-reset")
+    $("#west-sections, #center-sections").addClass("ui-accordion ui-widget ui-helper-reset")
     .find("h6")
         .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
         .prepend('<span class="ui-icon ui-icon-triangle-1-e"/>')
@@ -32,19 +32,6 @@ $(document).ready( function() {
         })
         .next().addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom").hide();
 
-
-    $("#center-sections").addClass("ui-accordion ui-widget ui-helper-reset")
-    .find("h6")
-        .addClass("ui-accordion-header ui-helper-reset ui-state-default ui-corner-top ui-corner-bottom")
-        .prepend('<span class="ui-icon ui-icon-triangle-1-e"/>')
-        .click(function() {
-            $(this).toggleClass("ui-accordion-header-active").toggleClass("ui-state-active")
-                .toggleClass("ui-state-default").toggleClass("ui-corner-bottom")
-            .find("> .ui-icon").toggleClass("ui-icon-triangle-1-e").toggleClass("ui-icon-triangle-1-s")
-            .end().next().toggleClass("ui-accordion-content-active").toggle();
-            return false;
-        })
-        .next().addClass("ui-accordion-content ui-helper-reset ui-widget-content ui-corner-bottom").hide();
 
     $("#map").click();
     $("#occupants").click();
