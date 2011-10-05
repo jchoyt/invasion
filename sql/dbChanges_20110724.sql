a1c74b175bdf20defc312fb1cbf245db82f78cfd
Jeffrey Hoyt <jchoyt@gmail.com>
2011-10-04 23:23:16 -0400

    db changes for melee weapons

--- sql/dbChanges_20110724.sql
+++ sql/dbChanges_20110724.sql
@@ -25,6 +25,8 @@ insert into effecttype ( name ) values ( 'Blacked Out' );
 insert into effecttype ( name ) values ( 'Insane' );
 insert into effecttype ( name ) values ( 'Healing' );
 
+update effecttype set id=id-1;
+
 create table effects (
     altid integer,
     effectid integer,
@@ -37,4 +39,30 @@ update statstype set statname='Kills' where statid=9;
 update statstype set statname='Deaths' where statid=10;
 insert into statstype (statid, statname) values (11,'Drunkenness');
 insert into statstype (statid, statname) values (12,'Black outs');
+
+alter table effects add constraint altid_fkey foreign key (altid) references alt(id);
+alter table effecttype add constraint effecttype_pkey primary key (id);
+alter table effects add constraint effectid_fkey foreign key (effectid) references effecttype(id);
+
+insert into skills (name, value, race, prerequisite, cost) values ( 'Melee 1', 131072, 'human', null, 10);
+insert into skills (name, value, race, prerequisite, cost) values ( 'Melee 2', 262144, 'human', 18, 20);
+insert into skills (name, value, race, prerequisite, cost) values ( 'Melee 3', 524288, 'human', 19, 40);
+insert into skills (name, value, race, prerequisite, cost) values ( 'Melee 4', 1048576, 'human', 20, 80);
+insert into skills (name, value, race, prerequisite, cost) values ( 'Melee 5', 2097152, 'human', 21, 160);
+
+
+
+/* CREATE OR REPLACE FUNCTION update_modified_column()
+	RETURNS TRIGGER AS $$
+	BEGIN
+	   NEW.modified = now();
+	   RETURN NEW;
+	END;
+	$$ language 'plpgsql';
+
+CREATE TRIGGER update_customer_modtime BEFORE UPDATE
+    ON customer FOR EACH ROW EXECUTE PROCEDURE
+    update_modified_column(); */
+
+
 
