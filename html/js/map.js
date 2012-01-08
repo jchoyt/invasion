e616d86765034b7b31f25ab7ec097a4af59af02c
Jeffrey Hoyt <jchoyt@gmail.com>
2012-01-08 07:59:37 -0500

    [B] Added note that all pets are feral [http://soulcubes.com/issues/54/click-critter-lh-pane-black-404-page]

--- html/js/map.js
+++ html/js/map.js
@@ -341,6 +341,11 @@ function showThrowItem(id)
     return false;
 }
 
+function show_pet_owner(id)
+{
+    $("#att-pet-box").html("This pet is feral.");
+};
+
 function show_pet_target(id)
 {
     for (var pet_index=0;  pet_index<window.lastPoll.pets.length; pet_index++)
@@ -354,7 +359,6 @@ function show_pet_target(id)
         }
     }
 };
-
 
 function recharge(id)
 {
